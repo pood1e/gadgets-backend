58fb6b30d44577d73570292c53062c253e6a65c8
pood1e <codingpood1e@gmail.com>
2025-06-28 22:02:33 +0800

    add clipboard module🎉

---
+++ business/clipboard/src/main/java/me/pood1e/gadgets/server/business/clipboard/ClipboardApplication.java
@@ -0,0 +1,16 @@
+package me.pood1e.gadgets.server.business.clipboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author pood1e
+ */
+@SpringBootApplication
+public class ClipboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClipboardApplication.class, args);
+    }
+
+}
